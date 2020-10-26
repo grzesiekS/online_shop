343557bacc0d64aaf7bb55e27bd972cfa547db7e
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-26 19:13:09 +0100

    Modify Splash component

--- src/components/features/Splash/Splash.js
+++ src/components/features/Splash/Splash.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './Splash.module.scss';
 
 const Splash = () => (
-  <div className={styles.container}>
-    <h1 className={styles.title}>Games for life</h1>
+  <section className={styles.splash}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Games for life</h1>
+    </div>
     <img className={styles.image} src='./images/splash_img.jpg' alt='play the game'/>
-  </div>
+  </section>
 );
 
 export default Splash;
