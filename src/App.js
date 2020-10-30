c452304e5a3ececb1fe023eda5371161e3f92080
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-30 11:25:46 +0100

    Implement Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './styles/global.scss';
 import MainLayout from './components/layout/MainLayout/MainLayout';
 import Homepage from './components/views/Homepage/HomepageContainer';
 
 const App = () => (
-  <MainLayout>
-    <Homepage />
-  </MainLayout>
+  <BrowserRouter>
+    <MainLayout>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
+    </MainLayout>
+  </BrowserRouter>
+
 );
 
 export default App;
