8b58d03ddf554024340483161031761787154b48
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-31 17:01:36 +0100

    implement randomImageSelection function in Game component

--- src/components/features/Game/Game.js
+++ src/components/features/Game/Game.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import {randomImageSelection} from '../../../utils/randomImage';
+
 import styles from './Game.module.scss';
 
 const Game = ({name, images, price, id}) => {
 
-  const randomImage = images[Math.floor(Math.random() * images.length)];
+  const rndImage = randomImageSelection(images);
 
   return (
     <div className={styles.container}>
       <Link to={`/game/${id}`}>
         <div className={styles.game}>
-          <img src={randomImage} alt='Game' />
+          <img src={rndImage} alt='Game' />
           <h2 className={styles.title}>{name}</h2>
           <p className={styles.price}>price start from <span className={styles.highlight}>{price} €</span></p>
         </div>
