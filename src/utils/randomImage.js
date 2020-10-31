0b124a02b8aa3924571c1605c80aa73aeb3f0eba
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-10-31 16:59:38 +0100

    Fix Error with isArray method

--- src/utils/randomImage.js
+++ src/utils/randomImage.js
@@ -1,5 +1,5 @@
-export const randomImage = imagesArray => {
-  if(imagesArray.isArray() && imagesArray.length > 0) {
+export const randomImageSelection = imagesArray => {
+  if(Array.isArray(imagesArray) && imagesArray.length > 0) {
     return imagesArray[Math.floor(Math.random() * imagesArray.length)];
   } else {
     return null;
