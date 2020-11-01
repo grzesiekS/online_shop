9ad56872fd0e6ea4e9f0a994cedb2e80039e98e1
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-01 18:49:43 +0100

    Create PhotoDisplay component and implement it in PhotoCarousel

---
+++ src/components/common/PhotoDisplay/PhotoDisplay.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './PhotoDisplay.module.scss';
+
+class PhotoDisplay extends React.Component {
+  state = {
+    photoStyle: [styles.photoDisplay],
+  }
+
+  componentDidUpdate = (prevProps) => {
+    if(prevProps.photo !== this.props.photo) {
+      console.log(prevProps.photo);
+      this.addPhotoStyle('active');
+      window.setTimeout(() => {this.removePhotoStyle('active');}, 250);
+    }
+  }
+
+  addPhotoStyle = style => {
+    this.setState({
+      ...this.state,
+      photoStyle: [
+        ...this.state.photoStyle,
+        styles[style],
+      ],
+    });
+  }
+
+  removePhotoStyle = style => {
+    this.setState({
+      ...this.state,
+      photoStyle: this.state.photoStyle.filter(pStyle => pStyle !== styles[style]),
+    });
+  }
+
+  render() {
+
+    const {photo} = this.props;
+
+    return (
+      <div className={this.state.photoStyle.join(' ')}>
+        <img className={styles.img} src={'.' + photo} alt='default' />
+      </div>
+    );
+  }
+}
+
+PhotoDisplay.propTypes = {
+  photo: PropTypes.string,
+};
+
+export default PhotoDisplay;
--- src/components/features/PhotoCarousel/PhotoCarousel.js
+++ src/components/features/PhotoCarousel/PhotoCarousel.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import styles from './PhotoCarousel.module.scss';
+import PhotoDisplay from '../../common/PhotoDisplay/PhotoDisplay';
 
 class PhotoCarousel extends React.Component {
   state = {
@@ -21,24 +22,27 @@ class PhotoCarousel extends React.Component {
     const {photos} = this.props;
 
     return (
-      <div className={styles.flexBox}>
-        {photos.map(photo => (
-          <div key={photos.indexOf(photo)}>
-            <img
-              className=
-                {
-                  photos.indexOf(photo) === this.state.activePhoto
-                    ?
-                    clsx(styles.img, styles.active)
-                    :
-                    clsx(styles.img)
-                }
-              src={'.' + photo}
-              alt='default'
-              onClick={() => this.changeActivePhoto(photos.indexOf(photo))}
-            />
-          </div>
-        ))}
+      <div className={styles.container}>
+        <PhotoDisplay photo={photos[this.state.activePhoto]} />
+        <div className={styles.flexBox}>
+          {photos.map(photo => (
+            <div key={photos.indexOf(photo)}>
+              <img
+                className=
+                  {
+                    photos.indexOf(photo) === this.state.activePhoto
+                      ?
+                      clsx(styles.img, styles.active)
+                      :
+                      clsx(styles.img)
+                  }
+                src={'.' + photo}
+                alt='default'
+                onClick={() => this.changeActivePhoto(photos.indexOf(photo))}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
