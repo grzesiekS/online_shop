9c12788d85b054bcb795386af43b9422b3acac0e
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-01 21:19:48 +0100

    Create Footer component, change hover on Game component

---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer = () => (
+  <div className={styles.container}>
+    <p>footer</p>
+  </div>
+);
+
+export default Footer;
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const MainLayout = ({children}) => (
   <div>
     <Header />
     {children}
+    <Footer />
   </div>
 );
 
