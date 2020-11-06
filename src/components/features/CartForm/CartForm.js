5d4d87b6fa1145e1b6764e8c107151f2b868c16c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-06 19:48:34 +0100

    Add button to close cartForm

--- src/components/features/CartForm/CartForm.js
+++ src/components/features/CartForm/CartForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './CartForm.module.scss';
 import Button from '../../common/Button/Button';
 
 const CartForm = () => (
   <div className={styles.cart}>
+    <div className={styles.closeWindow}>
+      <Button Type='div' className='icon'>
+        <FontAwesomeIcon icon={faTimes} className={styles.icon} />
+      </Button>
+    </div>
     <h1 className={styles.title}>Your order</h1>
     <form className={styles.cartForm}>
       <label htmlFor="name">Name</label>
