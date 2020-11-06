70fa1a6f1c58789005a6d9a1abb87df9632754c1
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-06 21:04:04 +0100

    Add style to disable bg when CartForm is active

--- src/components/features/PromptWindow/PromptWindow.js
+++ src/components/features/PromptWindow/PromptWindow.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import clsx from 'clsx';
 
 import styles from './PromptWindow.module.scss';
 import CartForm from '../CartForm/CartFormContainer';
 
 const PromptWindow = ({children, cartFormActiveStatus}) => (
-  <div className={styles.promptWindow}>
+  <div
+    className={
+      cartFormActiveStatus
+        ?
+        clsx(styles.promptWindow, styles.disableBg)
+        :
+        styles.promptWindow
+    }
+  >
     {cartFormActiveStatus
       ?
       <CartForm />
