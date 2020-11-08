bbeba2be9ec6965b37fde9253bc4aedcd8529da0
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-08 12:36:39 +0100

    Add Footer component

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 import styles from './Footer.module.scss';
+import Button from '../../common/Button/Button';
 
 const Footer = () => (
   <div className={styles.container}>
-    <p>footer</p>
+    <h2>Visit us on social medias</h2>
+    <ul className={styles.socialMedia}>
+      <li>
+        <Button href='/#' className='icon'>
+          <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+        </Button>
+      </li>
+      <li>
+        <Button href='/#' className='icon'>
+          <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+        </Button>
+      </li>
+      <li>
+        <Button href='/#' className='icon'>
+          <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+        </Button>
+      </li>
+      <li>
+        <Button href='/#' className='icon'>
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+        </Button>
+      </li>
+    </ul>
   </div>
 );
 
