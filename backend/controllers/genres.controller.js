f02ef8340d2900937fc1dcb74c5f1b8692ecf525
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-10 13:16:23 +0100

    Change genres controller

--- backend/controllers/genres.controller.js
+++ backend/controllers/genres.controller.js
@@ -2,7 +2,9 @@ const Gnere = require('../models/genres.model');
 
 exports.loadAll = async (req, res) => {
   try {
-    res.json(await Gnere.find());
+    const result = await Gnere.find();
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
   } catch(err) {
     res.status(500).json(err);
   }
