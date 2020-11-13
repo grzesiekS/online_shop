731bd2b19c09f8e5fee0f15cecb56be641241d0a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-13 13:13:47 +0100

    Modify CartForm component

--- src/components/features/CartForm/CartForm.js
+++ src/components/features/CartForm/CartForm.js
@@ -8,93 +8,93 @@ import styles from './CartForm.module.scss';
 import Button from '../../common/Button/Button';
 import CartItems from '../../features/CartItems/CartItemsContainer';
 
-const CartForm = ({changeActiveStatusCart, updateOrderDetails, ...props}) => {
 
-  const submitForm = () => {
-    if(props.name && props.lastname && props.email && props.phone && props.gamesInCartCount) {
-      props.changePromptStatus('success');
+class CartForm extends React.Component {
+
+  submitForm = () => {
+    if(this.props.name && this.props.lastname && this.props.email && this.props.phone && this.props.gamesInCartCount) {
+      this.props.changePromptStatus('success');
     } else {
-      props.changePromptStatus('error');
-      props.loadCartFromLocalStorage();
+      this.props.changePromptStatus('error');
+      this.props.loadCartFromLocalStorage();
     }
-  };
+  }
 
-  return (
-    <div className={styles.cart}>
-      <div className={styles.closeWindow}>
-        <Button Type='div' className='icon' onClick={() => changeActiveStatusCart()}>
-          <FontAwesomeIcon icon={faTimes} className={styles.icon} />
-        </Button>
+  componentDidUpdate() {
+    this.props.saveCartToLocalStorage(this.props.orderDetails);
+  }
+
+  render() {
+
+    return (
+      <div className={styles.cart}>
+        <div className={styles.closeWindow}>
+          <Button Type='div' className='icon' onClick={() => this.props.changeActiveStatusCart()}>
+            <FontAwesomeIcon icon={faTimes} className={styles.icon} />
+          </Button>
+        </div>
+        <h1 className={styles.title}>Your order</h1>
+        <CartItems />
+        <form className={styles.cartForm}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={this.props.name}
+            onChange={e =>
+              this.props.updateOrderDetails(
+                Object.keys(this.props).find(key => this.props[key] === this.props.name),
+                e.currentTarget.value
+              )}
+            required='required'
+          />
+          <label htmlFor="lastname">Last Name</label>
+          <input
+            id="lastname"
+            type="text"
+            value={this.props.lastname}
+            onChange={e =>
+              this.props.updateOrderDetails(
+                Object.keys(this.props).find(key => this.props[key] === this.props.lastname),
+                e.currentTarget.value
+              )}
+            required='required'
+          />
+          <label htmlFor="email">E-mail</label>
+          <input
+            id="email"
+            type="email"
+            value={this.props.email}
+            onChange={e =>
+              this.props.updateOrderDetails(
+                Object.keys(this.props).find(key => this.props[key] === this.props.email),
+                e.currentTarget.value
+              )}
+            required='required'
+          />
+          <label htmlFor="phone">Phone No.</label>
+          <input
+            id="phone"
+            type="tel"
+            value={this.props.phone}
+            onChange={e =>
+              this.props.updateOrderDetails(
+                Object.keys(this.props).find(key => this.props[key] === this.props.phone),
+                e.currentTarget.value
+              )}
+            required='required'
+          />
+          <Button
+            Type='div'
+            onClick={() => this.submitForm()}
+          >
+            Order Now
+          </Button>
+        </form>
       </div>
-      <h1 className={styles.title}>Your order</h1>
-      <CartItems />
-      <form className={styles.cartForm}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          type="text"
-          value={props.name}
-          onChange={e => {
-            updateOrderDetails(
-              Object.keys(props).find(key => props[key] === props.name),
-              e.currentTarget.value
-            );
-            props.saveCartToLocalStorage(props.orderDetails);
-          }}
-          required='required'
-        />
-        <label htmlFor="lastname">Last Name</label>
-        <input
-          id="lastname"
-          type="text"
-          value={props.lastname}
-          onChange={e => {
-            updateOrderDetails(
-              Object.keys(props).find(key => props[key] === props.lastname),
-              e.currentTarget.value
-            );
-            props.saveCartToLocalStorage(props.orderDetails);
-          }}
-          required='required'
-        />
-        <label htmlFor="email">E-mail</label>
-        <input
-          id="email"
-          type="email"
-          value={props.email}
-          onChange={e => {
-            updateOrderDetails(
-              Object.keys(props).find(key => props[key] === props.email),
-              e.currentTarget.value
-            );
-            props.saveCartToLocalStorage(props.orderDetails);
-          }}
-          required='required'
-        />
-        <label htmlFor="phone">Phone No.</label>
-        <input
-          id="phone"
-          type="tel"
-          value={props.phone}
-          onChange={e => {
-            updateOrderDetails(
-              Object.keys(props).find(key => props[key] === props.phone),
-              e.currentTarget.value
-            );
-            props.saveCartToLocalStorage(props.orderDetails);
-          }}
-          required='required'
-        />
-        <Button
-          Type='div'
-          onClick={() => submitForm()}
-        >
-          Order Now
-        </Button>
-      </form>
-    </div>
-  );
-};
+    );
+  }
+}
 
 CartForm.propTypes = {
   changeActiveStatusCart: PropTypes.func,
