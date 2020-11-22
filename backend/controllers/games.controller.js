4cbe606c48f7018615034bf00b81729930152ee4
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-22 12:03:37 +0100

    Modify games controlls and new control for new releases

--- backend/controllers/games.controller.js
+++ backend/controllers/games.controller.js
@@ -13,8 +13,20 @@ exports.loadAll = async (req, res) => {
 
 exports.loadSelected = async (req, res) => {
   try {
-    const result = await Game.findById(req.params.id);
+    const currentDate = new Date();
+    const result = await Game.find({$and : [{ _id: req.params.id }, { releaseDate: {$lte: currentDate} }]});
     if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.loadNewReleases = async (req, res) => {
+  try {
+    const currentDate = new Date();
+    const result = await Game.find({ releaseDate: {$gt: currentDate} });
+    if(!result) res.status(404).json({ games: 'Not found' });
     else res.json(result);
   } catch(err) {
     res.status(500).json(err);
--- backend/routes/games.routes.js
+++ backend/routes/games.routes.js
@@ -5,5 +5,6 @@ const games = require('../controllers/games.controller');
 
 router.get('/games', games.loadAll);
 router.get('/games/:id', games.loadSelected);
+router.get('/games/newRelease', games.loadNewReleases);
 
 module.exports = router;
