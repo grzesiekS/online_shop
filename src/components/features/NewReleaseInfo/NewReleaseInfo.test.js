746c82ded7004272885a540d14faa15d26d88aec
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-23 09:27:57 +0100

    Add simple test for NewReleaseInfo component

---
+++ src/components/features/NewReleaseInfo/NewReleaseInfo.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NewReleaseInfo from './NewReleaseInfo';
+
+describe('NewReleaseInfo component', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<NewReleaseInfo />);
+    expect(component).toBeTruthy();
+  });
+});
