c1e9908d778f11d31e438c10bac0a58caeb1db48
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-24 13:25:30 +0100

    Add min arg for games model

--- backend/models/games.model.js
+++ backend/models/games.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
   type: { type: String },
   name: { type: String, required: true},
-  price: { type: Number, required: true},
+  price: { type: Number, required: true, min: 0},
   photos: { type: Array },
   descriptiobn: { type: String, required: true },
   genres: { type: Array, required: true },
