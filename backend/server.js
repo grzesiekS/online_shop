fd6cdc16b2e798973196fef0d1831493a57dbeb4
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-25 12:28:56 +0100

    Add middleware static

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 /* API ENDPOINTS */
 app.use('/api', genresRoutes);
