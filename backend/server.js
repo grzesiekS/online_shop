7fe9d3c674ff62b779a3e35f10c84f4756d7f471
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-11-25 12:40:36 +0100

    Modify static middleware

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/images')));
 
 /* API ENDPOINTS */
 app.use('/api', genresRoutes);
